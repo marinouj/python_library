3d288caecdd7e59b5a4382b3e8f05ff54b1f25be
marinouj <marinoujoanna@gmail.com>
2023-07-11 19:29:00 +0100

    test

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='mypackage',
+    name='testpackage',
     version='0.0.1',    
     description='A simple Python package.',
     url='',
     author='Ioanna Marinou',
     author_email='marinoujoanna@gmail.com',
     license='MIT',
-    packages=['mypackage'],
+    packages=['testpackage'],
     install_requires=required,
     package_data={'': ['data/*.txt']},
     include_package_data=True,

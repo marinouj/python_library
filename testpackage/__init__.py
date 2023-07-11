158357bc8ae31bfc80f904a633ee5dae776b84df
marinouj <marinoujoanna@gmail.com>
2023-07-11 19:42:09 +0100

    test

--- testpackage/__init__.py
+++ testpackage/__init__.py
@@ -1,6 +1,6 @@
 import random
 import pkgutil
-
+import bpy
 class MyPackage:
     
     def __init__(self):
@@ -12,3 +12,8 @@ class MyPackage:
     def multiply(self, x, y):
         return x * y
     
+    def random(self):
+        return random.random()
+    
+    def test_bpy(self):
+        bpy.ops.import_scene.obj(filepath="data/Cube.obj")

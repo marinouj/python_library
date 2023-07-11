6e628ead066d131c96140246c3e63cb21cb9e073
marinouj <marinoujoanna@gmail.com>
2023-07-11 19:21:46 +0100

    temp1

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='kaedim_package',
+    name='mypackage',
     version='0.0.1',    
     description='A simple Python package.',
     url='',
-    author='Name Surname',
+    author='Ioanna Marinou',
     author_email='marinoujoanna@gmail.com',
     license='MIT',
-    packages=find_packages(),
+    packages=['mypackage'],
     install_requires=required,
     package_data={'': ['data/*.txt']},
     include_package_data=True,

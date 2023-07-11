e6938ba00876aacbb30b5c128711b52ef4649696
marinouj <marinoujoanna@gmail.com>
2023-07-11 19:46:15 +0100

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='testpackage',
-    version='0.0.1',    
+    version='0.0.2',    
     description='A simple Python package.',
     url='',
     author='Ioanna Marinou',
